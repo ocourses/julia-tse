f03330e26c3ab624d4375937c483ac45d699eaf6
Olivier Cots <olivier.cots@irit.fr>
2026-05-29 11:42:25 +0200

    Update to Julia 1.12: change Jupyter kernel from julia-1.11 to julia-1.12 in all .qmd files and _quarto.yml. Add quarto-preview.sh script for easier preview with venv activation. Fix activate.jl path references.

--- src/activate.jl
+++ src/activate.jl
@@ -1,9 +1,9 @@
 using Suppressor
 @suppress begin
     using Pkg
-    Pkg.activate(".")
+    Pkg.activate(@__DIR__)
     Pkg.instantiate()
-    include("../assets/julia/myshow.jl")
+    include(joinpath(@__DIR__, "..", "assets", "julia", "myshow.jl"))
     using Markdown
     using QuizQuestions
 end
